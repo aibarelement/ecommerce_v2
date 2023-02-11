af8a68f6b3d6b2c91be2768266d7edae8132a83e
a.bekkozhayev <n1Myw-2TimW_v-yC2bJ5>
2023-02-11 22:03:42 +0600

    send email

--- users/services.py
+++ users/services.py
@@ -2,10 +2,12 @@ import random
 import uuid
 from typing import Protocol, OrderedDict
 
+from django.conf import settings
 from django.core.cache import cache
 from rest_framework_simplejwt import tokens
+from templated_email import send_templated_mail
 
-from . import repos
+from . import models, repos
 
 
 class UserServicesInterface(Protocol):
@@ -23,46 +25,30 @@ class UserServicesV1:
     user_repos: repos.UserReposInterface = repos.UserReposV1()
 
     def create_user(self, data: OrderedDict) -> str:
-        code = self._generate_code()
-        session_id = str(uuid.uuid4())
-        cache.set(session_id, {**data, 'code': code}, timeout=300)
-        self._send_sms_to_phone_number(phone_number=data['phone_number'], code=code)
-
-        return session_id
+        return self._verify_phone_number(data=data)
 
     def verify_user(self, data: OrderedDict) -> None:
         user_data = cache.get(data['session_id'])
 
-        if not user_data:
-            return
-
-        if user_data['code'] != data['code']:
-            return
+        self._validate_session(data=user_data)
 
         user = self.user_repos.create_user(data={
             'email': user_data['email'],
             'phone_number': user_data['phone_number'],
         })
 
-        self._send_letter_to_email(email=user.email)
+        self._send_letter_to_email(user=user)
 
     def create_token(self, data: OrderedDict) -> str:
         user = self.user_repos.get_user(data=data)
-        code = self._generate_code()
-        session_id = str(uuid.uuid4())
-        cache.set(session_id, {'phone_number': str(user.phone_number), 'code': code}, timeout=300)
-        self._send_sms_to_phone_number(phone_number=data['phone_number'], code=code)
+        data['phone_number'] = str(user.phone_number)
 
-        return session_id
+        return self._verify_phone_number(data=data)
 
     def verify_token(self, data: OrderedDict) -> dict:
         user_data = cache.get(data['session_id'])
 
-        if not user_data:
-            return
-
-        if user_data['code'] != data['code']:
-            return
+        self._validate_session(data=user_data)
 
         user = self.user_repos.get_user(data={'phone_number': user_data['phone_number']})
 
@@ -74,14 +60,42 @@ class UserServicesV1:
             'refresh': str(refresh),
         }
 
+    def _verify_phone_number(self, data: OrderedDict) -> str:
+        code = self._generate_code()
+        session_id = str(uuid.uuid4())
+        cache.set(session_id, {**data, 'code': code}, timeout=300)
+        self._send_sms_to_phone_number(phone_number=data['phone_number'], code=code)
+
+        return session_id
+
     @staticmethod
-    def _send_letter_to_email(email: str) -> None:
-        print(f'send letter to {email}')
+    def _validate_session(data: OrderedDict):
+        if not data:
+            return
+
+        if data['code'] != data['code']:
+            return
+
+    @staticmethod
+    def _generate_code() -> str:
+        return ''.join(random.choices([str(i) for i in range(10)], k=4))
+
+    @staticmethod
+    def _send_letter_to_email(user: models.User) -> None:
+        send_templated_mail(
+            template_name='welcome',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[user.email],
+            context={
+                'email': user.email,
+                'phone_number': user.phone_number,
+            },
+        )
 
     @staticmethod
     def _send_sms_to_phone_number(phone_number: str, code: str) -> None:
         print(f'send sms code {code} to {phone_number}')
 
     @staticmethod
-    def _generate_code() -> str:
-        return ''.join(random.choices([str(i) for i in range(10)], k=4))
+    def _generate_session_id() -> str:
+        return str(uuid.uuid4())
